68123846756e7443c9be326f9d68faae87affe41
Evgenii Fedoseev <zorox112@yandex-team.ru>
2022-11-02 19:44:47 +0300

    docs(build): update

---
+++ docs/component---src-pages-404-js-a2371f6c927407553a0c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a2371f6c927407553a0c.js.map
