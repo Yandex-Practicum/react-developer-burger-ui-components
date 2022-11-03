e5af136f4cf8a6838108e792067ee2983b2d577f
Evgenii Fedoseev <zorox112@yandex-team.ru>
2022-11-03 10:03:30 +0300

    docs(theme): update

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,8 @@
 export default {
     typescript: true,
     base: '/react-developer-burger-ui-components/docs',
-    dest: '/docs'
+    dest: '/docs',
+    themeConfig: {
+        initialColorMode: 'dark',
+    },
 };
