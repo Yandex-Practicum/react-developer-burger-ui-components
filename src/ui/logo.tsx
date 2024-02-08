1a7682a4130f4a54febdb5ab76235f02a10c4d5d
Maxx Nikitin <maxxnikitin90@yandex.ru>
2024-02-08 17:36:24 +0700

    fix: update className prop in Logo

--- src/ui/logo.tsx
+++ src/ui/logo.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-export const Logo: FC<{ className: string }> = ({ className }) => {
+export const Logo: FC<{ className?: string }> = ({ className }) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
